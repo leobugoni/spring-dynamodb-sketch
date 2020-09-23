f636f42fe7c6c2bb785ceebb79cf2b1c607253d7
Leonardo Bugoni <leobugoni@gmail.com>
2020-09-23 15:50:08 -0300

    First Commit

---
+++ src/main/java/io/github/leobugoni/springdynamodbsketch/SpringDynamodbSketchApplication.java
@@ -0,0 +1,13 @@
+package io.github.leobugoni.springdynamodbsketch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDynamodbSketchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDynamodbSketchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leobugoni/springdynamodbsketch/SpringDynamodbSketchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leobugoni.springdynamodbsketch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDynamodbSketchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
